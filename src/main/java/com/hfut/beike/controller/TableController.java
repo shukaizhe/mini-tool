e11f039571a6b0d43e6943ee049deabe1ba9af7f
shukaizhe <1510711680@qq.com>
2023-01-29 16:32:30 +0800

    fix api

---
+++ src/main/java/com/hfut/beike/controller/TableController.java
@@ -0,0 +1,16 @@
+package com.hfut.beike.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname TableController
+ * @Description
+ * @Date 2023/1/29 16:06
+ * @Created by shukz
+ */
+@RestController
+@RequestMapping("table")
+public class TableController extends ApiController {
+
+}
