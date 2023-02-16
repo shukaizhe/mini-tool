81b34aa8bbce616364a920ed99e1dd8e13ef3edc
shukaizhe <1510711680@qq.com>
2023-02-16 16:50:51 +0800

    make buildSchema method

--- src/main/java/com/hfut/beike/config/CorsConfig.java
+++ src/main/java/com/hfut/beike/config/CorsConfig.java
@@ -1,8 +1,10 @@
 package com.hfut.beike.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * @Classname CorsConfig
@@ -11,21 +13,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Created by shukz
  */
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        //添加映射路径
-        registry.addMapping("/**")
-                //是否发送Cookie
-                .allowCredentials(true)
-                //设置放行哪些原始域   SpringBoot2.4.4下低版本使用.allowedOrigins("*")
-                .allowedOrigins("*")
-                //放行哪些请求方式
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                //.allowedMethods("*") //或者放行全部
-                //放行哪些原始请求头部信息
-                .allowedHeaders("*")
-                //暴露哪些原始请求头部信息
-                .exposedHeaders("*");
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        //1. 添加 CORS配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        //放行哪些原始域
+        config.addAllowedOrigin("*");
+        //是否发送 Cookie
+        config.setAllowCredentials(true);
+        //放行哪些请求方式
+        config.addAllowedMethod("*");
+        //放行哪些原始请求头部信息
+        config.addAllowedHeader("*");
+        //暴露哪些头部信息
+        config.addExposedHeader("*");
+        //2. 添加映射路径
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", config);
+        //3. 返回新的CorsFilter
+        return new CorsFilter(corsConfigurationSource);
     }
 }
