a977d904999734a5db32f2a5ba8c8c0534c8277d
shukaizhe <1510711680@qq.com>
2023-04-12 10:46:48 +0800

    remove hostor

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 北京、上海、广州、深圳、长春、成都、重庆、长沙、大连、福州、贵阳、呼和浩特、哈尔滨、合肥、杭州、海口、济南、昆明、拉萨、兰州、南京、宁波、南宁、青岛、苏州、沈阳、石家庄、天津、太原、武汉、乌鲁木齐、厦门、西安、西宁、银川、郑州
 
 ## 技术栈：
-Spring Boot / MyBatis / MySQL / WebMagic
+Spring Boot / MyBatis / MySQL / WebMagic / LiteFlow
 
 ## 进度
 未将爬取到的数据插入到MySQL数据库中。
--- src/main/java/com/hfut/beike/HouseApplication.java
+++ src/main/java/com/hfut/beike/HouseApplication.java
@@ -1,8 +1,6 @@
 package com.hfut.beike;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import net.hasor.spring.boot.EnableHasor;
-import net.hasor.spring.boot.EnableHasorWeb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +9,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 /**
  * @author Chenzh
  */
+
 /**
  * 启用Hasor
  */
-@EnableHasor    // 在Spring 中启用 Hasor
-@EnableHasorWeb // 将 hasor-web 配置到 Spring 环境中，Dataway 的 UI 是通过 hasor-web 提供服务。
 @EnableScheduling
 @MapperScan("com.hfut.beike.dao")
 @SpringBootApplication
